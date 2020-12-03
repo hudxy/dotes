ca2a9a9fcfafdc5c1e84be26df49706026bc3d7e
Hudson Dean <hudsonxy@gmail.com>
2020-12-02 20:38:30 -0800

    boilerplate Vue, Landing Page

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+    <img id="logo" src="./assets/dota2_logo.png">
+    <Landing msg="Welcome to Dotes"/>
+  </div>
+</template>
+
+<script>
+import Landing from './components/Landing.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Landing
+  }
+}
+</script>
+
+<style>
+*{
+  background-color: rgb(63, 63, 63);
+}
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: white;
+  margin-top: 0px;
+}
+#logo {
+  height: 200px;
+  width: 400px;
+}
+</style>
---
+++ src/components/Landing.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="landing">
+        <h1>{{msg}}</h1>
+        <input type="text" placeholder="Your Steam ID, scrub...">
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Landing',
+    props: {
+        msg: String
+    }
+}
+</script>
+
+
+<style scoped>
+.landing, input, select, textarea{
+    color: white;
+}
+</style>
